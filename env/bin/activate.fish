d566dcdf26c2b318f0a9276a923342d02208d5ff
shiwansh <shiwansh283@gmail.com>
2025-11-09 18:56:24 +0530

    Add homepage layout

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/shiwansh/website/tailwind_site/env
+set -gx VIRTUAL_ENV /home/shiwansh/techsite/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
